8c17c242b7693791984360b6dbd40412fcd8d3e2
huu-thien <86077619+huu-thien@users.noreply.github.com>
2023-04-13 09:49:23 +0700

    add user image action

--- app/src/main/java/com/AndroidFunitureShopApp/view/AccountFragment.java
+++ app/src/main/java/com/AndroidFunitureShopApp/view/AccountFragment.java
@@ -1,15 +1,30 @@
 package com.AndroidFunitureShopApp.view;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.Intent.getIntent;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,7 +32,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import com.AndroidFunitureShopApp.LoginActivity;
 import com.AndroidFunitureShopApp.MainActivity;
 import com.AndroidFunitureShopApp.R;
@@ -29,6 +43,10 @@ import com.AndroidFunitureShopApp.viewmodel.AccountAPIService;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
@@ -36,14 +54,17 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 public class AccountFragment extends Fragment {
     private FragmentAccountBinding binding;
     private Context context;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PICK_IMAGE_REQUEST = 2;
+    private static final int REQUEST_CAMERA_PERMISSION = 100;
+    private static final int REQUEST_GALLERY_PERMISSION = 200;
+
+
     CompositeDisposable compositeDisposable = new CompositeDisposable();
     AccountAPIService accountAPIService;
     int id = UserInfo.userInfo.getId();
-
     public AccountFragment(){
-
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,13 +72,34 @@ public class AccountFragment extends Fragment {
         if (getArguments() != null) {
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+        binding.selectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tạo dialog cho phép người dùng lựa chọn mở camera hoặc thư viện
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Chọn ảnh từ:");
+                String[] options = {"Máy ảnh", "Thư viện"};
+                builder.setItems(options, (dialog, which) -> {
+                    // Kiểm tra lựa chọn của người dùng
+                    if (which == 0) {
+                        // Chọn mở camera
+                        // Yêu cầu cấp quyền truy cập camera và/hoặc thư viện
+                        requestCameraPermission();
+                    } else if (which == 1) {
+                        // Chọn mở thư viện
+                        // Yêu cầu cấp quyền truy cập thư viện
+                        requestGalleryPermission();
+                    }
+                });
+                builder.show();
+            }
+        });
         ShowInfo();
         binding.btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,20 +110,95 @@ public class AccountFragment extends Fragment {
         });
         return view;
     }
-
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         this.context = context;
     }
 
+    // Phương thức yêu cầu cấp quyền truy cập camera và/hoặc thư viện
+    private void requestCameraPermission() {
+        // Yêu cầu cấp quyền truy cập camera và/hoặc thư viện
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CAMERA_PERMISSION);
+        } else {
+            // Nếu đã được cấp quyền truy cập camera và/hoặc thư viện
+            // Mở camera hoặc thư viện để lấy ảnh
+            dispatchTakePictureIntent(); // Mở camera
+            // pickImageFromGallery(); // Mở thư viện
+        }
+    }
+
+    // Phương thức yêu cầu cấp quyền truy cập thư viện
+    private void requestGalleryPermission() {
+        // Yêu cầu cấp quyền truy cập thư viện
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_GALLERY_PERMISSION);
+        } else {
+            // Nếu đã được cấp quyền truy cập thư viện
+            // Mở thư viện để chọn ảnh
+            pickImageFromGallery();
+        }
+    }
+
     public void ShowInfo(){
+        if(UserInfo.userInfo.getImageAva().length() >= 10) {
+            byte[] decodedString = Base64.decode(UserInfo.userInfo.getImageAva(), Base64.DEFAULT);
+            Bitmap decodedBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            binding.ciAvatar.setImageBitmap(decodedBitmap);
+        }
         if(UserInfo.userInfo.getUsername() != null) binding.etUsername.setText(UserInfo.userInfo.getUsername());
         if(UserInfo.userInfo.getFullName() != null) binding.etFullName.setText(UserInfo.userInfo.getFullName());
         if(UserInfo.userInfo.getPhone() != null) binding.etPhone.setText(UserInfo.userInfo.getPhone());
         if(UserInfo.userInfo.getEmail() != null) binding.etEmail.setText(UserInfo.userInfo.getEmail());
         if(UserInfo.userInfo.getDefaultAdress() != null) binding.etAddress.setText(UserInfo.userInfo.getDefaultAdress());
     }
+
+    // Phương thức chụp ảnh
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    // Phương thức chọn ảnh từ thư viện
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Nhận ảnh đã chụp và hiển thị trên ImageView
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            binding.ciAvatar.setImageBitmap(imageBitmap);
+        } else if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            // Nhận ảnh được chọn từ thư viện và hiển thị trên ImageView
+            Uri uri = data.getData();
+            try {
+                InputStream inputStream = getActivity().getContentResolver()
+                        .openInputStream(uri);
+                Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+                binding.ciAvatar.setImageBitmap(imageBitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     public void UpdateInfo(int Uid){
         accountAPIService = new AccountAPIService();
@@ -92,6 +209,14 @@ public class AccountFragment extends Fragment {
         String defaultAdress = binding.etAddress.getText().toString().trim();
         String phone = binding.etPhone.getText().toString().trim();
         String imageAva = "";
+        if(binding.ciAvatar.getDrawable() != null) {
+            Bitmap bitmap = ((BitmapDrawable) binding.ciAvatar.getDrawable()).getBitmap();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            imageAva = Base64.encodeToString(byteArray, Base64.DEFAULT);
+            Log.d("ImageAva: ", imageAva);
+        }
         if(TextUtils.isEmpty(fullname)){
             Toast.makeText(context, "Please, enter your Fullname!", Toast.LENGTH_SHORT).show();
         }   else if(TextUtils.isEmpty(password)){
@@ -151,13 +276,11 @@ public class AccountFragment extends Fragment {
         compositeDisposable.clear();
         binding = null;
     }
-
     //Menu Logout Action
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.logout_menu, menu);
     }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
